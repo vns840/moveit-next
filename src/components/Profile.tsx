e357587c15b5f5e86160ef4236335158f69bf071
vinicius <vinicius840_js@hotmail.com>
2021-02-27 09:25:14 -0300

    upload de arquivos finais

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+
+  const { level } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/vns840.png" alt="Vinicius José - vns840"/>
+      <div>
+        <strong>Vinicius José</strong>
+        
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level {level}
+        </p>
+      </div>
+    </div>
+  )
+}
